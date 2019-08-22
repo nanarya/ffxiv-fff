be1cba406944e6d08108061814d8b733b153eb10
nanarya <>
2019-08-22 18:27:37 +0900

    ぼちぼちいい感じに作ってみてる

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,44 +1,112 @@
-<template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+<template lang='pug'>
+  .wrapper
+
+    .targetCharacter
+      select(v-model='character1.server')
+        option(value='Garuda') Garuda
+        option(value='Ifrit') Ifrit
+        option(value='Bahamut') Bahamut
+      input(type='text' v-model='character1.firstName')
+      input(type='text' v-model='character1.lastName')
+      p
+        |{{ character1.data.Name }}
+        |ID:{{character1.data.ID}}
+        |Server:{{character1.data.Server}}
+      img(:src='character1.data.Avatar')
+
+    .targetCharacter
+      select(v-model='character1.server')
+        option(value='Garuda') Garuda
+        option(value='Ifrit') Ifrit
+        option(value='Bahamut') Bahamut
+      input(type='text' v-model='character2.firstName')
+      input(type='text' v-model='character2.lastName')
+      p
+        |{{ character2.data.Name }}
+        |ID:{{character2.data.ID}}
+        |Server:{{character2.data.Server}}
+      img(:src='character2.data.Avatar')
+
+    .div
+      button(@click='onClickButton')
+        |Character
+  
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String
+    // msg: String,
+  },
+  data() {
+    return {
+      character1: {
+        server: 'Garuda',
+        firstName: 'Kid',
+        lastName: 'Nanarya',
+        data: {},
+        friends: [],
+        error: false,
+      },
+      character2: {
+        server: 'Garuda',
+        firstName: 'Kid',
+        lastName: 'Nanarya',
+        data: {},
+        friends: [],
+        error: false,
+      }
+    }
+  },
+  methods: {
+    onClickButton () {
+      // console.log( this.character1LastName )
+      this.getCharacter(this.character1)
+      this.getCharacter(this.character2)
+    },
+    getCharacter (character) {
+      // console.log(`https://xivapi.com/character/search?name=${character.firstName}+${character.lastName}&server=${character.server}`)
+      fetch(`https://xivapi.com/character/search?name=${character.firstName}+${character.lastName}&server=${character.server}`, { mode: 'cors' })
+        .then(response => response.json())
+        .then(data => this.setCharacterData(character, data.Results[0]))
+    },
+
+    setCharacterData (character, data) {
+      // name check
+      if(`${character.firstName} ${character.lastName}` == data.Name) {
+        character.data = data
+        this.findFriends(character)
+      } else {
+        character.error = true
+      }
+    },
+    findFriends (character) {
+      fetch(`https://xivapi.com/character/${character.data.ID}?data=FR`, { mode: 'cors' })
+        .then(response => response.json())
+        .then(data => this.setFriends(character, data.Friends))
+      // console.log(this.character1.friends)
+    },
+    setFriends (character, friends) {
+      character.friends = friends
+      this.createFriendsFriendList()
+    },
+    createFriendsFriendList () {
+      let arr1 = this.character1.friends
+      let arr2 = this.character2.friends
+      
+      let allFriends = [...arr1, ...arr2]
+
+      let duplicatedArr = allFriends.filter(
+        item => arr1.includes(item) && arr2.includes(item)
+      )
+
+      console.log(duplicatedArr) // [ 'apple', 'pen', 'apple', 'pen' ]
+      console.log(new Set(duplicatedArr)) // Set { 'apple', 'pen' }
+    }
   }
 }
 </script>
-
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h3 {
